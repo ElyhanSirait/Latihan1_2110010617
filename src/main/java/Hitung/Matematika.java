973340d03878f1e7bf95ae81326eb14089669468
ElyhanSirait <elyhansirait@gmail.com>
2023-06-11 22:56:58 +0800

    Membuat constructor dan setPenjumlahan

--- src/main/java/Hitung/Matematika.java
+++ src/main/java/Hitung/Matematika.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Hitung;
 
-/**
- *
- * @author User
- */
 public class Matematika {
-    
-}
+    //variabel
+   double bil1, bil2; 
+   
+   //constructor
+   public Matematika(double bil1, double bil2) {
+       this.bil1 = bil1;
+       this.bil2 = bil2;
+       }
+       //method setPenjumlahan
+       public double setPenjumlahan(){
+        return bil1 + bil2;
+       }
+    }
